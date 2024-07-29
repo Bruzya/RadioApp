940bacfbfb63b4109ab70a1ee8642921e1d31ff8
Evgenii Mazrukho <mazrukho@gmail.com>
2024-07-29 17:09:09 +0300

    add files

---
+++ RadioApp/App/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  RadioApp
+//
+//  Created by Evgenii Mazrukho on 29.07.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let vc = OnboardingVC()
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ RadioApp/Modules/Onboarding/OnboardingVC.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingVC.swift
+//  RadioApp
+//
+//  Created by Evgenii Mazrukho on 29.07.2024.
+//
+
+import UIKit
+
+class OnboardingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
