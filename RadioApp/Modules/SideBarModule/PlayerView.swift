3dde63f3b04da647cddb414865be7f05dd9cfcdc
Vladislav Golyakov <justry24@gmail.com>
2024-08-04 00:37:25 +0300

    add stationDetailsVC

--- RadioApp/Modules/SideBarModule/PlayerView.swift
+++ RadioApp/Modules/SideBarModule/PlayerView.swift
@@ -13,7 +13,7 @@ import SnapKit
 
 
 final class PlayerView: UIView {
-
+    
     private let hStack: UIStackView = {
         let stack = UIStackView()
         stack.axis = .horizontal
@@ -21,52 +21,50 @@ final class PlayerView: UIView {
         stack.distribution = .equalSpacing
         return stack
     }()
-
+    
     private let leftButton: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(resource: .previous)
         image.contentMode = .scaleAspectFit
         return image
     }()
-
+    
     private let playButton: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(resource: .player)
         image.contentMode = .scaleAspectFit
         return image
     }()
-
+    
     private let rightButton: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(resource: .next)
         image.contentMode = .scaleAspectFit
         return image
     }()
-
-    private let frequencyBarsView = FrequencyBarsView()
-
+    
+    
     var audioPlayer: AVPlayer?
     var timer: Timer?
     var volumeSlider: UISlider!
-
+    
     private let disposeBag = DisposeBag()
-
+    
     init() {
         super.init(frame: .zero)
         if let url = URL(string: "http://icecast.vgtrk.cdnvideo.ru/vestifm_mp3_192kbps") {
             audioPlayer = AVPlayer(url: url)
             print("Player initialized: \(String(describing: audioPlayer))")
         }
-
+        
         setupUI()
         setupBindings()
-        startAudioProcessing()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     func setupBindings() {
         playButton.rx.tapGesture()
             .when(.recognized)
@@ -76,7 +74,7 @@ final class PlayerView: UIView {
                 animate(with: playButton)
             })
             .disposed(by: disposeBag)
-
+        
         leftButton.rx.tapGesture()
             .when(.recognized)
             .mapToVoid()
@@ -84,7 +82,7 @@ final class PlayerView: UIView {
                 animate(with: leftButton)
             })
             .disposed(by: disposeBag)
-
+        
         rightButton.rx.tapGesture()
             .when(.recognized)
             .mapToVoid()
@@ -92,34 +90,21 @@ final class PlayerView: UIView {
                 animate(with: rightButton)
             })
             .disposed(by: disposeBag)
-
+        
         volumeSlider.rx.value
             .bind(onNext: { [unowned self] value in
                 audioPlayer?.volume = Float(value)
             })
             .disposed(by: disposeBag)
     }
-
+    
     private func togglePlayPause() {
-         if audioPlayer?.rate == 0 {
-             audioPlayer?.play()
-             print("Play sound")
-         } else {
-             print("pause")
-             audioPlayer?.pause()
-         }
-     }
-
-    private func startAudioProcessing() {
-        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
-            guard let self, audioPlayer?.rate != 0 else { return }
-            let magnitudes = AudioProcessor.sharedInstance.frequencyMagnitudes
-            DispatchQueue.main.async {
-                UIView.animate(withDuration: 0.1) {
-                    self.frequencyBarsView.updateBars(with: magnitudes)
-                }
-            }
-            print(magnitudes)
+        if audioPlayer?.rate == 0 {
+            audioPlayer?.play()
+            print("Play sound")
+        } else {
+            print("pause")
+            audioPlayer?.pause()
         }
     }
 }
@@ -145,27 +130,13 @@ private extension PlayerView {
                 hStack.addArrangedSubview($0)
             }
 
-        [frequencyBarsView, hStack, volumeSlider]
+        [hStack, volumeSlider]
             .forEach {
             addSubview($0)
         }
     }
 
     func setupConstraints() {
-        
-        frequencyBarsView.snp.makeConstraints { make in
-            make
-                .bottom
-                .equalTo(hStack.snp.top)
-            make
-                .leading.trailing
-                .equalToSuperview()
-                .inset(20)
-            make
-                .height
-                .equalTo(100)
-        }
-
         hStack.snp.makeConstraints { make in
             make
                 .top.leading.trailing
--- RadioApp/Modules/StationDetailsScreen/StationDetailsVC.swift
+++ RadioApp/Modules/StationDetailsScreen/StationDetailsVC.swift
@@ -5,4 +5,141 @@
 //  Created by Evgenii Mazrukho on 29.07.2024.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+import AVFoundation
+
+final class StationDetailsVC: UIViewController {
+    
+    var onDismiss = PublishRelay<Void>()
+    
+    private let backButton: UIButton = {
+        let button = UIButton()
+        button.setImage(.back, for: .normal)
+        return button
+    }()
+    
+    private let frequencyBarsView = FrequencyBarsView()
+    
+    private let favoritesButton: UIButton = {
+        let button = UIButton()
+        button.tintColor = .white
+        return button
+    }()
+    
+    private let radioLabel: UILabel = {
+        let label = UILabel()
+        label.text = "90.5"
+        label.font = UIFont.systemFont(ofSize: 36, weight: .heavy)
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let stationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Radio Divelement"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
+
+    var audioPlayer: AVPlayer?
+    
+    private var isFavorite = false
+
+    private let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        startAudioProcessing()
+        setupBindings()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        onDismiss.accept(())
+    }
+    
+    private func setupBindings() {
+        backButton.rx.tap
+            .mapToVoid()
+            .bind(onNext: { [weak self] in
+                self?.navigationController?.popViewController(animated: false)
+            })
+            .disposed(by: disposeBag)
+        
+        favoritesButton.rx.tap
+            .bind(onNext: { [weak self] in
+                guard let self = self else { return }
+                self.isFavorite.toggle()
+                let imageName: ImageResource = self.isFavorite ? .likeFilled : .like
+                self.favoritesButton.setImage(UIImage(resource: imageName).withRenderingMode(.alwaysTemplate), for: .normal)
+                self.favoritesButton.tintColor = self.isFavorite ? .red : .white
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func startAudioProcessing() {
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
+            guard let self, audioPlayer?.rate != 0 else { return }
+            let magnitudes = AudioProcessor.sharedInstance.frequencyMagnitudes
+            DispatchQueue.main.async {
+                self.frequencyBarsView.updateBars(with: magnitudes)
+            }
+            print(magnitudes)
+        }
+    }
+}
+
+private extension StationDetailsVC {
+    func setupUI() {
+        favoritesButton.setImage(UIImage(resource: isFavorite ? .likeFilled : .like).withRenderingMode(.alwaysTemplate), for: .normal)
+        favoritesButton.tintColor = isFavorite ? .red : .white
+        view.backgroundColor = Colors.background
+        navigationController?.navigationBar.isHidden = true
+        addSubviews()
+        setupConstraints()
+    }
+    
+    func addSubviews() {
+        view.addSubview(backButton)
+        view.addSubview(favoritesButton)
+        view.addSubview(frequencyBarsView)
+        view.addSubview(radioLabel)
+        view.addSubview(stationLabel)
+    }
+    
+    func setupConstraints() {
+        backButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(20)
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.size.equalTo(30)
+        }
+        
+        favoritesButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(20)
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.size.equalTo(30)
+        }
+        
+        radioLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(50)
+            make.leading.trailing.equalToSuperview().inset(20)
+        }
+        
+        stationLabel.snp.makeConstraints { make in
+            make.top.equalTo(radioLabel.snp.bottom)
+            make.leading.trailing.equalToSuperview().inset(20)
+        }
+        
+        frequencyBarsView.snp.makeConstraints { make in
+            make.top.equalTo(stationLabel.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(400)
+        }
+    }
+}
