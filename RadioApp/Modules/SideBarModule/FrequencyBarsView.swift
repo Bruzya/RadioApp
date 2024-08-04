031aad7416d7d574f31bbf9a9edee8cd7290ba9b
Vladislav Golyakov <justry24@gmail.com>
2024-08-04 13:23:56 +0300

    add background

--- RadioApp/Modules/SideBarModule/FrequencyBarsView.swift
+++ RadioApp/Modules/SideBarModule/FrequencyBarsView.swift
@@ -42,79 +42,68 @@ final class FrequencyBarsView: UIView {
     }
 
     func updateBars(with magnitudes: [Float]) {
-        guard magnitudes.count > 1 else { return }
+        UIView.animate(withDuration: 0.1) { [unowned self] in
+            guard magnitudes.count > 1 else { return }
 
-        // Interpolate between current magnitudes and new magnitudes
-        let interpolatedMagnitudes = interpolateMagnitudes(currentMagnitudes, newMagnitudes: magnitudes)
-        currentMagnitudes = magnitudes
+            currentMagnitudes = magnitudes
 
-        // Remove existing point layers
-        pointLayers.forEach { $0.removeFromSuperlayer() }
-        pointLayers.removeAll()
+            // Remove existing point layers
+            pointLayers.forEach { $0.removeFromSuperlayer() }
+            pointLayers.removeAll()
 
-        let width = self.bounds.width
-        let height = self.bounds.height
-        let centerY = height / 2
-        let barWidth = width / CGFloat(magnitudes.count - 1)
-        let pointRadius: CGFloat = 2.0 // Radius of each point
-        let maxMagnitude: Float = 2.0
-        let maxSpacing: CGFloat = 1 // Maximum spacing between points
+            let width = self.bounds.width
+            let height = self.bounds.height
+            let centerY = height / 2
+            let barWidth = width / CGFloat(magnitudes.count - 1)
+            let pointRadius: CGFloat = 2.0 // Radius of each point
+            let maxMagnitude: Float = 1.5
+            let maxSpacing: CGFloat = 2 // Maximum spacing between points
 
-        for (index, magnitude) in interpolatedMagnitudes.enumerated() {
-            let x = CGFloat(index) * barWidth
-            let clampedMagnitude = min(magnitude, maxMagnitude)
-            let normalizedMagnitude = clampedMagnitude / maxMagnitude
-            let availableHeight = CGFloat(normalizedMagnitude) * (height / 2)
-            let spacing = 2 * pointRadius + maxSpacing
-            let numPoints = Int(availableHeight / spacing)
+            for (index, magnitude) in magnitudes.enumerated() {
+                let x = CGFloat(index) * barWidth
+                let clampedMagnitude = min(magnitude, maxMagnitude)
+                let normalizedMagnitude = clampedMagnitude / maxMagnitude
+                let availableHeight = CGFloat(normalizedMagnitude) * (height / 2)
+                let spacing = 2 * pointRadius + maxSpacing
+                let numPoints = Int(availableHeight / spacing)
 
-            for i in 0..<numPoints {
-                let yTop = centerY - CGFloat(i) * spacing
-                let yBottom = centerY + CGFloat(i) * spacing
+                for i in 0..<numPoints {
+                    let yTop = centerY - CGFloat(i) * spacing
+                    let yBottom = centerY + CGFloat(i) * spacing
 
-                // Create a circle path for the top point
-                let pointPath = UIBezierPath(arcCenter: CGPoint(x: pointRadius, y: pointRadius), radius: pointRadius, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
+                    // Create a circle path for the top point
+                    let pointPath = UIBezierPath(arcCenter: CGPoint(x: pointRadius, y: pointRadius), radius: pointRadius, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
 
-                // Create a shape layer for the top point
-                let shapeLayerTop = CAShapeLayer()
-                shapeLayerTop.path = pointPath.cgPath
-                shapeLayerTop.frame = CGRect(x: x - pointRadius, y: yTop - pointRadius, width: pointRadius * 2, height: pointRadius * 2)
-                shapeLayerTop.fillColor = UIColor.black.cgColor
+                    // Create a shape layer for the top point
+                    let shapeLayerTop = CAShapeLayer()
+                    shapeLayerTop.path = pointPath.cgPath
+                    shapeLayerTop.frame = CGRect(x: x - pointRadius, y: yTop - pointRadius, width: pointRadius * 2, height: pointRadius * 2)
+                    shapeLayerTop.fillColor = UIColor.black.cgColor
 
-                // Create a shape layer for the bottom point
-                let shapeLayerBottom = CAShapeLayer()
-                shapeLayerBottom.path = pointPath.cgPath
-                shapeLayerBottom.frame = CGRect(x: x - pointRadius, y: yBottom - pointRadius, width: pointRadius * 2, height: pointRadius * 2)
-                shapeLayerBottom.fillColor = UIColor.black.cgColor
+                    // Create a shape layer for the bottom point
+                    let shapeLayerBottom = CAShapeLayer()
+                    shapeLayerBottom.path = pointPath.cgPath
+                    shapeLayerBottom.frame = CGRect(x: x - pointRadius, y: yBottom - pointRadius, width: pointRadius * 2, height: pointRadius * 2)
+                    shapeLayerBottom.fillColor = UIColor.black.cgColor
 
-                // Add the shape layers to the gradient layer's mask
-                self.layer.addSublayer(shapeLayerTop)
-                self.layer.addSublayer(shapeLayerBottom)
-                pointLayers.append(shapeLayerTop)
-                pointLayers.append(shapeLayerBottom)
+                    // Add the shape layers to the gradient layer's mask
+                    pointLayers.append(shapeLayerTop)
+                    pointLayers.append(shapeLayerBottom)
+                }
             }
-        }
 
-        // Mask the gradient layer with the combined shape layers
-        let combinedMaskLayer = CALayer()
-        pointLayers.forEach { combinedMaskLayer.addSublayer($0) }
-        gradientLayer?.mask = combinedMaskLayer
-    }
-
-    private func interpolateMagnitudes(_ currentMagnitudes: [Float], newMagnitudes: [Float]) -> [Float] {
-        guard currentMagnitudes.count == newMagnitudes.count else { return newMagnitudes }
-        let interpolationFactor: Float = 0.1 // Adjust this value for smoother or faster interpolation
-        return zip(currentMagnitudes, newMagnitudes).map { current, new in
-            current + (new - current) * interpolationFactor
+            // Mask the gradient layer with the combined shape layers
+            let combinedMaskLayer = CALayer()
+            pointLayers.forEach { combinedMaskLayer.addSublayer($0) }
+            gradientLayer?.mask = combinedMaskLayer
         }
     }
 
     @objc private func updateAnimation() {
-        let width = self.bounds.width
         let height = self.bounds.height
         let centerY = height / 2
         let amplitude: CGFloat = 180 // Amplitude of the wave
-        let frequency: CGFloat = 3 // Frequency of the wave
+        let frequency: CGFloat = 2 // Frequency of the wave
 
         for layer in pointLayers {
             let x = layer.frame.midX
--- RadioApp/Modules/StationDetailsScreen/StationDetailsVC.swift
+++ RadioApp/Modules/StationDetailsScreen/StationDetailsVC.swift
@@ -12,23 +12,23 @@ import SnapKit
 import AVFoundation
 
 final class StationDetailsVC: UIViewController {
-    
+
     var onDismiss = PublishRelay<Void>()
-    
+
     private let backButton: UIButton = {
         let button = UIButton()
         button.setImage(.back, for: .normal)
         return button
     }()
-    
+
     private let frequencyBarsView = FrequencyBarsView()
-    
+
     private let favoritesButton: UIButton = {
         let button = UIButton()
         button.tintColor = .white
         return button
     }()
-    
+
     private let radioLabel: UILabel = {
         let label = UILabel()
         label.text = "90.5"
@@ -37,7 +37,7 @@ final class StationDetailsVC: UIViewController {
         label.textAlignment = .center
         return label
     }()
-    
+
     private let stationLabel: UILabel = {
         let label = UILabel()
         label.text = "Radio Divelement"
@@ -47,23 +47,31 @@ final class StationDetailsVC: UIViewController {
         return label
     }()
 
+    private let backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(resource: .background)
+        return imageView
+    }()
+
     var audioPlayer: AVPlayer?
-    
+
     private var isFavorite = false
 
     private let disposeBag = DisposeBag()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         startAudioProcessing()
         setupBindings()
     }
-    
+
     override func viewDidDisappear(_ animated: Bool) {
         onDismiss.accept(())
     }
-    
+
     private func setupBindings() {
         backButton.rx.tap
             .mapToVoid()
@@ -71,7 +79,7 @@ final class StationDetailsVC: UIViewController {
                 self?.navigationController?.popViewController(animated: false)
             })
             .disposed(by: disposeBag)
-        
+
         favoritesButton.rx.tap
             .bind(onNext: { [weak self] in
                 guard let self = self else { return }
@@ -82,7 +90,7 @@ final class StationDetailsVC: UIViewController {
             })
             .disposed(by: disposeBag)
     }
-    
+
     private func startAudioProcessing() {
         Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
             guard let self, audioPlayer?.rate != 0 else { return }
@@ -90,7 +98,6 @@ final class StationDetailsVC: UIViewController {
             DispatchQueue.main.async {
                 self.frequencyBarsView.updateBars(with: magnitudes)
             }
-            print(magnitudes)
         }
     }
 }
@@ -104,38 +111,43 @@ private extension StationDetailsVC {
         addSubviews()
         setupConstraints()
     }
-    
+
     func addSubviews() {
+        view.addSubview(backgroundImageView)
         view.addSubview(backButton)
         view.addSubview(favoritesButton)
         view.addSubview(frequencyBarsView)
         view.addSubview(radioLabel)
         view.addSubview(stationLabel)
     }
-    
+
     func setupConstraints() {
+        backgroundImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+
         backButton.snp.makeConstraints { make in
             make.leading.equalToSuperview().inset(20)
             make.top.equalTo(view.safeAreaLayoutGuide)
             make.size.equalTo(30)
         }
-        
+
         favoritesButton.snp.makeConstraints { make in
             make.trailing.equalToSuperview().inset(20)
             make.top.equalTo(view.safeAreaLayoutGuide)
             make.size.equalTo(30)
         }
-        
+
         radioLabel.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(50)
             make.leading.trailing.equalToSuperview().inset(20)
         }
-        
+
         stationLabel.snp.makeConstraints { make in
             make.top.equalTo(radioLabel.snp.bottom)
             make.leading.trailing.equalToSuperview().inset(20)
         }
-        
+
         frequencyBarsView.snp.makeConstraints { make in
             make.top.equalTo(stationLabel.snp.bottom).offset(20)
             make.leading.trailing.equalToSuperview()
