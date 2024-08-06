d5dfb31f5f2d8ea2bcf2a01d83fd7b799df6e0c7
Artem Tarasov <artemdrolllted@gmail.com>
2024-08-06 23:15:57 +0300

    fix scene delegate

--- RadioApp/App/SceneDelegate.swift
+++ RadioApp/App/SceneDelegate.swift
@@ -16,17 +16,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-//        appDIContainer = AppDIContainer()
-//        appCoordinator = AppCoordinator(
-//            navigationController: UINavigationController(),
-//            window: window,
-//            appDIContainer: appDIContainer
-//        )
-//        
-//        appCoordinator?.start()
-        let vc = SettingsVC()
-        window?.makeKeyAndVisible()
-        window?.rootViewController = UINavigationController(rootViewController: vc)
+        appDIContainer = AppDIContainer()
+        appCoordinator = AppCoordinator(
+            navigationController: UINavigationController(),
+            window: window,
+            appDIContainer: appDIContainer
+        )
+        
+        appCoordinator?.start()
 
     }
 }
